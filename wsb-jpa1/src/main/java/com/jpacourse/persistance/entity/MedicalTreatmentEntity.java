f2fbb97a62e196e730090d7330dd46415b81d0fa
Kamil <KamilArkit@interia.pl>
2025-05-17 10:04:09 +0200

    Poprawiłem formatowanie i usunąłem niepotrzebne importy w celu lepszej czytelności.

--- wsb-jpa1/src/main/java/com/jpacourse/persistance/entity/MedicalTreatmentEntity.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistance/entity/MedicalTreatmentEntity.java
@@ -1,56 +1,55 @@
 package com.jpacourse.persistance.entity;
 
 import com.jpacourse.persistance.enums.TreatmentType;
-
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "MEDICAL_TREATMENT")
 public class MedicalTreatmentEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(nullable = false)
-	private String description;
+    @Column(nullable = false)
+    private String description;
 
-	@Enumerated(EnumType.STRING)
-	private TreatmentType type;
+    @Enumerated(EnumType.STRING)
+    private TreatmentType type;
 
-	@ManyToOne
-	@JoinColumn(name = "visit_id", nullable = false)
-	private VisitEntity visit;
+    @ManyToOne
+    @JoinColumn(name = "visit_id", nullable = false)
+    private VisitEntity visit;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public TreatmentType getType() {
-		return type;
-	}
+    public TreatmentType getType() {
+        return type;
+    }
 
-	public void setType(TreatmentType type) {
-		this.type = type;
-	}
+    public void setType(TreatmentType type) {
+        this.type = type;
+    }
 
-	public VisitEntity getVisit() {
-		return visit;
-	}
+    public VisitEntity getVisit() {
+        return visit;
+    }
 
-	public void setVisit(VisitEntity visit) {
-		this.visit = visit;
-	}
+    public void setVisit(VisitEntity visit) {
+        this.visit = visit;
+    }
 }
--- wsb-jpa1/src/main/java/com/jpacourse/persistance/entity/VisitEntity.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistance/entity/VisitEntity.java
@@ -1,81 +1,80 @@
 package com.jpacourse.persistance.entity;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "VISIT")
 public class VisitEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	private String description;
+    private String description;
 
-	@Column(nullable = false)
-	private LocalDateTime time;
+    @Column(nullable = false)
+    private LocalDateTime time;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "DOCTOR_ID", referencedColumnName = "ID")
-	private DoctorEntity doctor;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DOCTOR_ID", referencedColumnName = "ID")
+    private DoctorEntity doctor;
 
-	@ManyToOne()
-	@JoinColumn(name = "PATIENT_ID")
-	private PatientEntity patient;
+    @ManyToOne()
+    @JoinColumn(name = "PATIENT_ID")
+    private PatientEntity patient;
 
-	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-	@JoinColumn(name = "VISIT_ID")
-	private List<MedicalTreatmentEntity> treatments;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "VISIT_ID")
+    private List<MedicalTreatmentEntity> treatments;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public LocalDateTime getTime() {
-		return time;
-	}
+    public LocalDateTime getTime() {
+        return time;
+    }
 
-	public void setTime(LocalDateTime time) {
-		this.time = time;
-	}
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
 
-	public DoctorEntity getDoctor() {
-		return doctor;
-	}
+    public DoctorEntity getDoctor() {
+        return doctor;
+    }
 
-	public void setDoctor(DoctorEntity doctor) {
-		this.doctor = doctor;
-	}
+    public void setDoctor(DoctorEntity doctor) {
+        this.doctor = doctor;
+    }
 
-	public PatientEntity getPatient() {
-		return patient;
-	}
+    public PatientEntity getPatient() {
+        return patient;
+    }
 
-	public void setPatient(PatientEntity patient) {
-		this.patient = patient;
-	}
+    public void setPatient(PatientEntity patient) {
+        this.patient = patient;
+    }
 
-	public List<MedicalTreatmentEntity> getTreatments() {
-		return treatments;
-	}
+    public List<MedicalTreatmentEntity> getTreatments() {
+        return treatments;
+    }
 
-	public void setTreatments(List<MedicalTreatmentEntity> treatments) {
-		this.treatments = treatments;
-	}
+    public void setTreatments(List<MedicalTreatmentEntity> treatments) {
+        this.treatments = treatments;
+    }
 }
