ad380abdb84a278c98a84a33732664c3b7318fca
dominik <dominik585@poczta.fm>
2025-05-23 19:41:55 +0200

    LAB3 - Zrefaktoryzowalem testy dla PatientDaoTest oraz dodalem testy sprawdzajace wyszukiwanie pacjenta po nazwisku oraz pacjentow ktorzy maja wiecej niz 2 wizyty.

--- wsb-jpa1/src/test/java/com/jpacourse/persistance/dao/PatientDaoTest.java
+++ wsb-jpa1/src/test/java/com/jpacourse/persistance/dao/PatientDaoTest.java
@@ -1,17 +1,19 @@
 package com.jpacourse.persistance.dao;
 
-import com.jpacourse.persistance.entity.AddressEntity;
+import com.jpacourse.persistance.builders.TestDoctorBuilder;
+import com.jpacourse.persistance.builders.TestPatientBuilder;
+import com.jpacourse.persistance.builders.TestVisitBuilder;
 import com.jpacourse.persistance.entity.DoctorEntity;
 import com.jpacourse.persistance.entity.PatientEntity;
-import com.jpacourse.persistance.enums.Specialization;
+import com.jpacourse.persistance.entity.VisitEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,18 +25,22 @@ public class PatientDaoTest {
     @Autowired
     private DoctorDao doctorDao;
     @Autowired
+    private VisitDao visitDao;
+    @Autowired
     private AddressDao addressDao;
 
     @Transactional
     @Test
     public void shouldCreateVisitForPatient() {
         //given
-        PatientEntity testPatient = createTestPatient();
-        AddressEntity testAddress = createTestAddress();
-        DoctorEntity testDoctor = createTestDoctor(testAddress);
+        PatientEntity testPatient = TestPatientBuilder.defaultPatient().build();
+        DoctorEntity testDoctor = TestDoctorBuilder.defaultDoctor().build();
         LocalDateTime dateOfVisit = LocalDateTime.of(2025, 5, 5, 16, 15, 0);
         int amountOfVisitsBeforeInsert = testPatient.getVisits() != null ? testPatient.getVisits().size() : 0;
+
         //when
+        patientDao.save(testPatient);
+        doctorDao.save(testDoctor);
         patientDao.createVisitForPatient(testPatient.getId(), testDoctor.getId(), dateOfVisit, "testVisitDesc");
 
         //then
@@ -42,37 +48,76 @@ public class PatientDaoTest {
         assertThat(testPatient.getVisits().size()).isEqualTo(amountOfVisitsBeforeInsert + 1);
     }
 
-    private PatientEntity createTestPatient() {
-        PatientEntity patient = new PatientEntity();
-        patient.setFirstName("Anna");
-        patient.setLastName("Nowak");
-        patient.setTelephoneNumber("777123321");
-        patient.setEmail("anna.nowak@example.com");
-        patient.setPatientNumber("PAT-001");
-        patient.setDateOfBirth(LocalDate.of(1990, 5, 15));
-        patient.setDateOfJoin(LocalDateTime.of(2025, 5, 16, 12, 12, 12));
-        return patientDao.save(patient);
+    @Transactional
+    @Test
+    public void shouldFindTwoPatientsWithLastNameEqualNowak() {
+        //given
+        PatientEntity testPatient1 = TestPatientBuilder.defaultPatient()
+                .withFirstName("Joanna")
+                .withLastName("Nowak")
+                .build();
+        PatientEntity testPatient2 = TestPatientBuilder.defaultPatient()
+                .withFirstName("Adam")
+                .withLastName("Nowak")
+                .withEmail("adam.Nowak@test.com")
+                .withTelephoneNumber("777123321")
+                .withPatientNumber("PAT-001")
+                .build();
+
+        //when
+        patientDao.save(testPatient1);
+        patientDao.save(testPatient2);
+        List<PatientEntity> patientsWithLastNameNowak = patientDao.findByLastName("Nowak");
+
+        //then
+        assertThat(patientsWithLastNameNowak).isNotNull();
+        assertThat(patientsWithLastNameNowak.size()).isEqualTo(2);
     }
 
-    private AddressEntity createTestAddress() {
-        AddressEntity address = new AddressEntity();
-        address.setCity("Warszawa");
-        address.setAddressLine1("Zielna 6");
-        address.setPostalCode("12-123");
+    @Transactional
+    @Test
+    public void shouldFindPatientsWithMoreThanTwoVisits() {
+        //given
+        PatientEntity testPatient1 = TestPatientBuilder.defaultPatient().build();
+        PatientEntity testPatient2 = TestPatientBuilder.defaultPatient()
+                .withFirstName("Jan")
+                .withLastName("Testowy")
+                .build();
+        DoctorEntity testDoctor = TestDoctorBuilder.defaultDoctor().build();
+        VisitEntity visit1 = TestVisitBuilder.defaultVisit()
+                .withPatient(testPatient1)
+                .withDoctor(testDoctor)
+                .build();
+        VisitEntity visit2 = TestVisitBuilder.defaultVisit()
+                .withPatient(testPatient1)
+                .withDoctor(testDoctor)
+                .withTime(LocalDateTime.now().plusDays(11))
+                .build();
+        VisitEntity visit3 = TestVisitBuilder.defaultVisit()
+                .withPatient(testPatient1)
+                .withDoctor(testDoctor)
+                .withTime(LocalDateTime.now().plusDays(96))
+                .build();
+        VisitEntity visit4 = TestVisitBuilder.defaultVisit()
+                .withPatient(testPatient2)
+                .withDoctor(testDoctor)
+                .withTime(LocalDateTime.now().plusDays(34))
+                .build();
 
-        return addressDao.save(address);
-    }
+        //when
+        addressDao.save(testPatient1.getAddress());
+        patientDao.save(testPatient1);
+        patientDao.save(testPatient2);
+        addressDao.save(testDoctor.getAddress());
+        doctorDao.save(testDoctor);
+        visitDao.save(visit1);
+        visitDao.save(visit2);
+        visitDao.save(visit3);
+        visitDao.save(visit4);
+        List<PatientEntity> patientsWithMoreVisitsThan = patientDao.findPatientsWithMoreVisitsThan(2);
 
-    private DoctorEntity createTestDoctor(AddressEntity address) {
-        DoctorEntity doctor = new DoctorEntity();
-        doctor.setFirstName("Jan");
-        doctor.setLastName("Kowalski");
-        doctor.setTelephoneNumber("777777777");
-        doctor.setEmail("JanKow@gmail.com");
-        doctor.setDoctorNumber("DOC001");
-        doctor.setSpecialization(Specialization.DERMATOLOGIST);
-        doctor.setAddress(address);
-
-        return doctorDao.save(doctor);
+        //then
+        assertThat(patientsWithMoreVisitsThan).isNotNull();
+        assertThat(patientsWithMoreVisitsThan.size()).isEqualTo(1);
     }
 }
