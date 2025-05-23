3531aebd2bf0df4b1cdc01f35c439a7636ac6cb7
dominik <dominik585@poczta.fm>
2025-05-23 19:39:51 +0200

    LAB3 - Zrefaktoryzowalem testy dla PatientServiceTest aby uzywaly nowego API.

--- wsb-jpa1/src/test/java/com/jpacourse/persistance/service/PatientServiceTest.java
+++ wsb-jpa1/src/test/java/com/jpacourse/persistance/service/PatientServiceTest.java
@@ -1,13 +1,17 @@
 package com.jpacourse.persistance.service;
 
 import com.jpacourse.dto.PatientTO;
+import com.jpacourse.mapper.PatientMapper;
+import com.jpacourse.persistance.builders.TestDoctorBuilder;
+import com.jpacourse.persistance.builders.TestPatientBuilder;
+import com.jpacourse.persistance.builders.TestVisitBuilder;
 import com.jpacourse.persistance.dao.AddressDao;
 import com.jpacourse.persistance.dao.DoctorDao;
 import com.jpacourse.persistance.dao.PatientDao;
 import com.jpacourse.persistance.dao.VisitDao;
-import com.jpacourse.persistance.entity.*;
-import com.jpacourse.persistance.enums.Specialization;
-import com.jpacourse.persistance.enums.TreatmentType;
+import com.jpacourse.persistance.entity.DoctorEntity;
+import com.jpacourse.persistance.entity.PatientEntity;
+import com.jpacourse.persistance.entity.VisitEntity;
 import com.jpacourse.service.DoctorService;
 import com.jpacourse.service.PatientService;
 import com.jpacourse.service.VisitService;
@@ -17,9 +21,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -46,99 +49,55 @@ public class PatientServiceTest {
     @Test
     public void shouldDeletePatientAndAssociatedVisitsButPreserveDoctors() {
         //given
-        AddressEntity address = createTestAddress();
-        DoctorEntity doctor = createTestDoctor(address);
-        PatientEntity patient = createTestPatient();
-        VisitEntity visit1 = createTestVisit(patient, doctor, 5);
-        VisitEntity visit2 = createTestVisit(patient, doctor, 10);
-        patient.setVisits(List.of(visit1, visit2));
-        Long patientId = patient.getId();
-        Long doctorId = doctor.getId();
-
+        VisitEntity testVisit1 = TestVisitBuilder.defaultVisit().build();
+        VisitEntity testVisit2 = TestVisitBuilder.defaultVisit().build();
+        PatientEntity testPatient = TestPatientBuilder.defaultPatient()
+                .withVisits(Arrays.asList(testVisit1, testVisit2))
+                .build();
+        DoctorEntity testDoctor = TestDoctorBuilder.defaultDoctor().build();
         //when
-        patientService.deleteById(patientId);
+        addressDao.save(testPatient.getAddress());
+        patientDao.save(testPatient);
+        visitDao.save(testVisit1);
+        visitDao.save(testVisit2);
+        addressDao.save(testDoctor.getAddress());
+        doctorDao.save(testDoctor);
+
+        patientService.deleteById(testPatient.getId());
 
         //then
-        assertThat(visitService.findVisitsByPatientId(patientId).size()).isEqualTo(0);
-        assertThat(doctorService.findById(doctorId)).isNotNull();
+        assertThat(visitService.findVisitsByPatientId(testPatient.getId()).size()).isEqualTo(0);
+        assertThat(doctorService.findById(testDoctor.getId())).isNotNull();
     }
 
     @Test
     @Transactional
     public void shouldReturnApprovedPatientToStructure() {
         //given
-        AddressEntity address = createTestAddress();
-        DoctorEntity doctor = createTestDoctor(address);
-        PatientEntity patient = createTestPatient();
-        VisitEntity visit1 = createTestVisit(patient, doctor, 5);
-        VisitEntity visit2 = createTestVisit(patient, doctor, 10);
-        patient.setVisits(List.of(visit1, visit2));
-
+        VisitEntity testVisit1 = TestVisitBuilder.defaultVisit().build();
+        VisitEntity testVisit2 = TestVisitBuilder.defaultVisit().build();
+        PatientEntity testPatient = TestPatientBuilder.defaultPatient()
+                .withVisits(Arrays.asList(testVisit1, testVisit2))
+                .build();
         //when
-        PatientTO patientTO = patientService.findById(patient.getId());
+        addressDao.save(testPatient.getAddress());
+        patientDao.save(testPatient);
+        visitDao.save(testVisit1);
+        visitDao.save(testVisit2);
+
+        PatientTO patientTO = patientService.findById(testPatient.getId());
 
         //then
-        assertThat(patient.getId()).isEqualTo(patientTO.getId());
-        assertThat(patient.getFirstName()).isEqualTo(patientTO.getFirstName());
-        assertThat(patient.getLastName()).isEqualTo(patientTO.getLastName());
-        assertThat(patient.getTelephoneNumber()).isEqualTo(patientTO.getTelephoneNumber());
-        assertThat(patient.getEmail()).isEqualTo(patientTO.getEmail());
-        assertThat(patient.getPatientNumber()).isEqualTo(patientTO.getPatientNumber());
-        assertThat(patient.getDateOfBirth()).isEqualTo(patientTO.getDateOfBirth());
-        assertThat(patient.getDateOfJoin()).isEqualTo(patientTO.getDateOfJoin());
-        assertThat(patient.getVisits().size()).isEqualTo(2);
+        assertThat(testPatient.getId()).isEqualTo(patientTO.getId());
+        assertThat(testPatient.getFirstName()).isEqualTo(patientTO.getFirstName());
+        assertThat(testPatient.getLastName()).isEqualTo(patientTO.getLastName());
+        assertThat(testPatient.getTelephoneNumber()).isEqualTo(patientTO.getTelephoneNumber());
+        assertThat(testPatient.getEmail()).isEqualTo(patientTO.getEmail());
+        assertThat(testPatient.getPatientNumber()).isEqualTo(patientTO.getPatientNumber());
+        assertThat(testPatient.getDateOfBirth()).isEqualTo(patientTO.getDateOfBirth());
+        assertThat(testPatient.getDateOfJoin()).isEqualTo(patientTO.getDateOfJoin());
+        assertThat(testPatient.getVisits().size()).isEqualTo(2);
         assertThat(patientTO.getVisitEntities().size()).isEqualTo(0);
-    }
-
-    private AddressEntity createTestAddress() {
-        AddressEntity address = new AddressEntity();
-        address.setCity("Warszawa");
-        address.setAddressLine1("Zielna 6");
-        address.setPostalCode("12-123");
-
-        return addressDao.save(address);
-    }
-
-    private DoctorEntity createTestDoctor(AddressEntity address) {
-        DoctorEntity doctor = new DoctorEntity();
-        doctor.setFirstName("Jan");
-        doctor.setLastName("Kowalski");
-        doctor.setTelephoneNumber("777777777");
-        doctor.setEmail("JanKow@gmail.com");
-        doctor.setDoctorNumber("DOC001");
-        doctor.setSpecialization(Specialization.DERMATOLOGIST);
-        doctor.setAddress(address);
-
-        return doctorDao.save(doctor);
-    }
-
-    private PatientEntity createTestPatient() {
-        PatientEntity patient = new PatientEntity();
-        patient.setFirstName("Anna");
-        patient.setLastName("Nowak");
-        patient.setTelephoneNumber("777123321");
-        patient.setEmail("anna.nowak@example.com");
-        patient.setPatientNumber("PAT-001");
-        patient.setDateOfBirth(LocalDate.of(1990, 5, 15));
-        patient.setDateOfJoin(LocalDateTime.of(2025, 5, 16, 12, 12, 12));
-        return patientDao.save(patient);
-    }
-
-    private VisitEntity createTestVisit(PatientEntity patient, DoctorEntity doctor, int plusDays) {
-
-        VisitEntity visit = new VisitEntity();
-        visit.setPatient(patient);
-        visit.setDoctor(doctor);
-        visit.setTime(LocalDateTime.now().plusDays(plusDays));
-        visit.setMedicalTreatments(List.of(createMedicalTreatmentEntity()));
-        return visitDao.save(visit);
-    }
-
-    public MedicalTreatmentEntity createMedicalTreatmentEntity() {
-        MedicalTreatmentEntity medicalTreatmentEntity = new MedicalTreatmentEntity();
-        medicalTreatmentEntity.setDescription("testDesc");
-        medicalTreatmentEntity.setType(TreatmentType.RTG);
-        return medicalTreatmentEntity;
     }
 
 }
